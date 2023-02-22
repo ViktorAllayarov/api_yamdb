d493c40c1b93fcd7afeb96e9e8456c3ca5cb3bc5
Viktor Oblomov <viktorall88@gmail.com.com>
2023-02-23 02:22:09 +0300

    реализован ендпоинт /auth/token/

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -10,3 +10,19 @@ class AuthSignupSerializer(serializers.ModelSerializer):
             "username",
         )
         model = User
+
+
+class GetJWTTokenSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    confirmation_code = serializers.CharField(required=True)
+
+    def validate(self, data):
+        if not User.objects.filter(username=data.get("username")).exists():
+            raise serializers.ValidationError("Ошибка: не верный username")
+        if not User.objects.filter(
+            password=data.get("confirmation_code")
+        ).exists():
+            raise serializers.ValidationError(
+                "Ошибка: не верный confirmation_code"
+            )
+        return data
